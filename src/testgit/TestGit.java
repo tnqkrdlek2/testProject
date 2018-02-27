bb17f17d5168909774fb6e7965482f3c3e9365de
tnqkrdlek2 <tnqkrdlek2@naver.com>
2018-02-27 17:31:01 +0900

    System.out.println()+++

--- src/testgit/TestGit.java
+++ src/testgit/TestGit.java
@@ -2,5 +2,11 @@ package testgit;
 public class TestGit {
 	public static void main(String [] args) {
 		System.out.println("헬롱 ㅋㅋ 월드 ");
+		System.out.println("아 산업기사 공부하기 싫다");
+		System.out.println("아 산업기사 공부하기 싫다");
+		System.out.println("아 산업기사 공부하기 싫다");
+		System.out.println("아 산업기사 공부하기 싫다");
+		
+		
 	}
 }
